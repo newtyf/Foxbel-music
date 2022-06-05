dd8e7f575640b9db86df35659b9a858b3dcf4141
Axel Muñoz <axel.munoz@picnic.pe>
2022-06-04 20:24:33 -0500

    init convert pwa

---
+++ src/spotify.js
@@ -0,0 +1,34 @@
+var request = require('request'); // "Request" library
+
+var client_id = 'a4ca84f81d3546f181850abc502cceb3'; // Your client id
+var client_secret = '64bb1e832dc24a0981034d6817ec758a'; // Your secret
+
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
